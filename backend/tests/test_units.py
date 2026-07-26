3f231445849ea93f0fedf73ec6fc1cb3b5b83cd6
cresbot15 <108076529+cresbot15@users.noreply.github.com>
2026-07-27 09:25:49 +1000

    Remove unnecessary auth_headers() arguments

--- backend/tests/test_units.py
+++ backend/tests/test_units.py
@@ -6,7 +6,7 @@ TEST_UNIT_CODE = "unit_code"
 TEST_UNIT_NAME = "unit_name"
 
 def test_create_unit(client, auth_headers):
-    headers = auth_headers(email=TEST_USER_EMAIL, password = TEST_USER_PASSWORD)
+    headers = auth_headers()
 
     request_body = {
         "name": TEST_UNIT_NAME,
@@ -22,7 +22,7 @@ def test_create_unit(client, auth_headers):
     assert response_body["name"] == TEST_UNIT_NAME
 
 def test_create_duplicate_unit(client, auth_headers, create_unit):
-    headers = auth_headers(email=TEST_USER_EMAIL, password=TEST_USER_PASSWORD)
+    headers = auth_headers()
 
     create_unit(headers=headers, code=TEST_UNIT_CODE, name=TEST_UNIT_NAME)
 
