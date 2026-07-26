6024872894c2c39ce19b77e28363a0a9c4e444f2
cresbot15 <108076529+cresbot15@users.noreply.github.com>
2026-07-26 20:12:24 +1000

    Add some negative auth tests

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -12,17 +12,52 @@ def test_register_creates_user(client):
     })
 
     assert response.status_code == 200
-    body = response.json()
-    assert body["email"] == TEST_USER_EMAIL
-    assert body["group_id"] is None
-    assert body["units"] == []
+    response_body = response.json()
+    assert response_body["email"] == TEST_USER_EMAIL
+
+    # A newly created user should not be part of a group or have any units
+    assert response_body["group_id"] is None
+    assert response_body["units"] == []
 
 def test_register_login(client, make_user):
     make_user(email=TEST_USER_EMAIL, password=TEST_USER_PASSWORD)
 
-    response = client.post("/auth/login", json={"email": TEST_USER_EMAIL, "password": TEST_USER_PASSWORD})
+    request_body = {
+        "email": TEST_USER_EMAIL,
+        "password": TEST_USER_PASSWORD
+    }
+
+    response = client.post("/auth/login", json=request_body)
 
     assert response.status_code == 200
-    body = response.json()
-    assert "access_token" in body
-    assert body["token_type"] == "bearer"
+    response_body = response.json()
+    assert "access_token" in response_body
+    assert response_body["token_type"] == "bearer"
+
+def test_register_login_wrong_email(client, make_user):
+    make_user(email=TEST_USER_EMAIL, password=TEST_USER_PASSWORD)
+
+    request_body = {
+        "email": "wrong@email.com",
+        "password": TEST_USER_PASSWORD
+    }
+    
+    response = client.post("/auth/login", json=request_body)
+
+    assert response.status_code == 401
+    # Login endpoint should not distinguish between incorrect username or password
+    assert response.json()["detail"] == "Invalid login details"
+
+def test_register_login_wrong_password(client, make_user):
+    make_user(email=TEST_USER_EMAIL, password=TEST_USER_PASSWORD)
+
+    request_body = {
+        "email": TEST_USER_EMAIL,
+        "password": "wrong_password"
+    }
+    
+    response = client.post("/auth/login", json=request_body)
+
+    assert response.status_code == 401
+    # Login endpoint should not distinguish between incorrect username or password
+    assert response.json()["detail"] == "Invalid login details"
